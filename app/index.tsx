ac2a03515186ebc31e1d1718b73c953519ede114
Matt-ethan <matthewethan6@gmail.com>
2024-09-15 03:01:05 +0800

    initialize repository

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Image, StyleSheet, Platform } from 'react-native';
+
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { View ,Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View><Text>THANKS</Text></View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
